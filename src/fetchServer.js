96ecb30d6fe9c3f1189ae2b1e9499a560367ba64
Jesse <jesse.t.crouch@gmail.com>
2020-05-17 10:54:54 -0400

    Changed active port number and backend server URL to match production.

--- src/fetchServer.js
+++ src/fetchServer.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 
-var server = 'http://10.0.0.177:3500';
+var server = 'http://cosgrovehockeyacademy.com:5460';
 $.get(server + '/api/serverURL', (result) => {
     if (result.server) {
         server = result.server;
